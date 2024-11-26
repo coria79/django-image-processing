e6dd08d62a0554a99e17ee5159947c3c9355a1d8
coria79 <coria79@gmail.com>
2024-11-26 17:24:53 +0100

    Procfile test 7

--- project_image_processing/project_image_processing/wsgi.py
+++ project_image_processing/project_image_processing/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_image_processing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_image_processing.project_image_processing.settings')
 
 application = get_wsgi_application()
